3c7b254aaf8ebf4fb28091564437bc7782241dc9
xAlexCode <alexandra.henriksson00@gmail.com>
2026-06-14 20:36:44 +0200

    added placeholder data to project array, added interface and fullscreen mode in own file

--- src/scripts/renderProjects.ts
+++ src/scripts/renderProjects.ts
@@ -1,10 +1,10 @@
-import { projects } from '../data/projects';
+import { projects } from "../data/projects";
 
 export function renderProjects() {
-    const container = document.querySelector(".projectContainer") as HTMLElement;
+  const container = document.querySelector(".projectContainer") as HTMLElement;
 
-    projects.forEach(project => {
-        container.innerHTML += `
+  projects.forEach((project) => {
+    container.innerHTML += `
            <div class="projectCard">
                <h3>${project.title}</h3>
                <p>${project.description}</p>
@@ -14,5 +14,5 @@ export function renderProjects() {
                </div>
            </div>
         `;
-    });
+  });
 }
